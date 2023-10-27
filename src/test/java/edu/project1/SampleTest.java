9c2f55da7c79eb5e8c239a1bb79fdc0f962d89d2
Nikita Kosh <kosheevnikita80@gmail.com>
2023-10-27 17:57:52 +0300

    upgrade byte-buddy version to 1.14.8

--- src/test/java/edu/project1/SampleTest.java
+++ src/test/java/edu/project1/SampleTest.java
@@ -3,6 +3,7 @@ package edu.project1;
 import edu.hw2.EvenArrayUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SampleTest {
