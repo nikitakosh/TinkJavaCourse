545016eef21df61e094a5afcfb43b23724a9d476
Nikita Kosh <kosheevnikita80@gmail.com>
2023-10-28 19:09:03 +0300

    add  interfaces Generator, Render, Solver, records Cell, Coordinate, class Maze

---
+++ src/main/java/edu/project2/Render.java
@@ -0,0 +1,8 @@
+package edu.project2;
+
+import java.util.List;
+
+public interface Render {
+    String render(Maze maze);
+    String render(Maze maze, List<Coordinate> path);
+}
--- src/main/java/edu/project2/Temp.java
+++ src/main/java/edu/project2/Temp.java
@@ -4,5 +4,6 @@ public class Temp {
     public static void main(String[] args) {
         Main main = new Main();
         main.build();
+        System.out.println("0x00002381");
     }
 }
