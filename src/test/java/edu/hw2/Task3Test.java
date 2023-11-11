23da59114926eae2fd8c444063134e42033e03b7
Nikita Kosh <kosheevnikita80@gmail.com>
2023-11-11 20:05:44 +0300

    add mockito dependency

--- src/test/java/edu/hw2/Task3Test.java
+++ src/test/java/edu/hw2/Task3Test.java
@@ -3,16 +3,12 @@ package edu.hw2;
 import edu.hw2.task3.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 
 import java.util.stream.Stream;
-
-import static org.mockito.ArgumentMatchers.anyString;
 
 
 public class Task3Test {
