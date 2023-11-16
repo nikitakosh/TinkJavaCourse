7ca5f983f7e00a1b5c2bfc599a75889eada030f2
Nikita Kosh <kosheevnikita80@gmail.com>
2023-11-16 14:50:03 +0300

    use Set instead of List at AbstractFilterTest

--- src/test/java/edu/hw6/AbstractFilterTest.java
+++ src/test/java/edu/hw6/AbstractFilterTest.java
@@ -7,21 +7,21 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class AbstractFilterTest {
 
     @Test
     @DisplayName("larger than")
     public void largerThan() {
-        List<Path> paths = new ArrayList<>();
+        Set<Path> paths = new HashSet<>();
         try (DirectoryStream<Path> entries = Files.newDirectoryStream(Path.of("src/main/java/edu/hw6/Task3/filesForTest/"), AbstractFilter.largerThan(100000))) {
             entries.forEach(paths::add);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        Assertions.assertEquals(paths, List.of(
+        Assertions.assertEquals(paths, Set.of(
                 Path.of("src/main/java/edu/hw6/Task3/filesForTest/bigImage.png")
         ));
     }
@@ -29,13 +29,13 @@ public class AbstractFilterTest {
     @Test
     @DisplayName("is file readable")
     public void isReadable() {
-        List<Path> paths = new ArrayList<>();
+        Set<Path> paths = new HashSet<>();
         try (DirectoryStream<Path> entries = Files.newDirectoryStream(Path.of("src/main/java/edu/hw6/Task3/filesForTest/"), AbstractFilter.IS_READABLE_FILE)) {
             entries.forEach(paths::add);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        Assertions.assertEquals(paths, List.of(
+        Assertions.assertEquals(paths, Set.of(
                 Path.of("src/main/java/edu/hw6/Task3/filesForTest/bigImage.png"),
                 Path.of("src/main/java/edu/hw6/Task3/filesForTest/fileHelloJavaWorld.txt"),
                 Path.of("src/main/java/edu/hw6/Task3/filesForTest/fileHelloKotlinWorld.txt"),
@@ -46,13 +46,13 @@ public class AbstractFilterTest {
     @Test
     @DisplayName("is file writable")
     public void isWritable() {
-        List<Path> paths = new ArrayList<>();
+        Set<Path> paths = new HashSet<>();
         try (DirectoryStream<Path> entries = Files.newDirectoryStream(Path.of("src/main/java/edu/hw6/Task3/filesForTest/"), AbstractFilter.IS_WRITABLE_FILE)) {
             entries.forEach(paths::add);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        Assertions.assertEquals(paths, List.of(
+        Assertions.assertEquals(paths, Set.of(
                 Path.of("src/main/java/edu/hw6/Task3/filesForTest/bigImage.png"),
                 Path.of("src/main/java/edu/hw6/Task3/filesForTest/fileHelloJavaWorld.txt"),
                 Path.of("src/main/java/edu/hw6/Task3/filesForTest/fileHelloKotlinWorld.txt"),
@@ -63,13 +63,13 @@ public class AbstractFilterTest {
     @Test
     @DisplayName("glob matches")
     public void globMatches() {
-        List<Path> paths = new ArrayList<>();
+        Set<Path> paths = new HashSet<>();
         try (DirectoryStream<Path> entries = Files.newDirectoryStream(Path.of("src/main/java/edu/hw6/Task3/filesForTest/"), AbstractFilter.globMatches("*.png"))) {
             entries.forEach(paths::add);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        Assertions.assertEquals(paths, List.of(
+        Assertions.assertEquals(paths, Set.of(
                 Path.of("src/main/java/edu/hw6/Task3/filesForTest/bigImage.png"),
                 Path.of("src/main/java/edu/hw6/Task3/filesForTest/smallImage.png")
         ));
@@ -78,13 +78,13 @@ public class AbstractFilterTest {
     @Test
     @DisplayName("contains regex")
     public void containsRegex() {
-        List<Path> paths = new ArrayList<>();
+        Set<Path> paths = new HashSet<>();
         try (DirectoryStream<Path> entries = Files.newDirectoryStream(Path.of("src/main/java/edu/hw6/Task3/filesForTest/"), AbstractFilter.containsRegex("^file.*"))) {
             entries.forEach(paths::add);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        Assertions.assertEquals(paths, List.of(
+        Assertions.assertEquals(paths, Set.of(
                 Path.of("src/main/java/edu/hw6/Task3/filesForTest/fileHelloJavaWorld.txt"),
                 Path.of("src/main/java/edu/hw6/Task3/filesForTest/fileHelloKotlinWorld.txt")
         ));
@@ -93,14 +93,14 @@ public class AbstractFilterTest {
     @Test
     @DisplayName("contains magic numbers")
     public void magicNumbers() {
-        List<Path> paths = new ArrayList<>();
+        Set<Path> paths = new HashSet<>();
         try (DirectoryStream<Path> entries = Files.newDirectoryStream(Path.of("src/main/java/edu/hw6/Task3/filesForTest/"), AbstractFilter.magicNumber(0x89, 'P', 'N', 'G'))) {
             entries.forEach(paths::add);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        Assertions.assertEquals(paths, List.of(
+        Assertions.assertEquals(paths, Set.of(
                 Path.of("src/main/java/edu/hw6/Task3/filesForTest/bigImage.png"),
                 Path.of("src/main/java/edu/hw6/Task3/filesForTest/smallImage.png")
         ));
@@ -109,13 +109,13 @@ public class AbstractFilterTest {
     @Test
     @DisplayName("multiple filter")
     public void and() {
-        List<Path> paths = new ArrayList<>();
+        Set<Path> paths = new HashSet<>();
         try (DirectoryStream<Path> entries = Files.newDirectoryStream(Path.of("src/main/java/edu/hw6/Task3/filesForTest/"), AbstractFilter.globMatches("*.png").and(AbstractFilter.containsRegex("^small.*")))) {
             entries.forEach(paths::add);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        Assertions.assertEquals(paths, List.of(
+        Assertions.assertEquals(paths, Set.of(
                 Path.of("src/main/java/edu/hw6/Task3/filesForTest/smallImage.png")
         ));
     }
