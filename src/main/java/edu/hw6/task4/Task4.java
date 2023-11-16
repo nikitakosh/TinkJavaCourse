01c9156f54189ae2fafb9151c24831ba079df47a
Nikita Kosh <kosheevnikita80@gmail.com>
2023-11-16 14:13:36 +0300

    change package at Task4.java

--- src/main/java/edu/hw6/task4/Task4.java
+++ src/main/java/edu/hw6/task4/Task4.java
@@ -1,16 +1,16 @@
 package edu.hw6.task4;
 
 import java.io.BufferedOutputStream;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.zip.CRC32;
 import java.util.zip.CheckedOutputStream;
-import org.assertj.core.util.Files;
 
 public class Task4 {
 
@@ -20,8 +20,13 @@ public class Task4 {
     }
 
     public static void compositeOutputStream() {
-        File file = Files.newFile("src/main/java/edu/hw6/task4/" + "file.txt");
-        try (OutputStream outputStream = new FileOutputStream(file);
+        Path path;
+        try {
+            path = Files.createFile(Path.of("src/main/java/edu/hw6/task4/", "file.txt"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (OutputStream outputStream = new FileOutputStream(path.toFile());
              CheckedOutputStream checkedOutputStream = new CheckedOutputStream(outputStream, new CRC32());
              BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
              OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8);
